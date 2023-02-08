89ef759eeb35c602c79181e049da42523b4c8c2b
John Elesho <oluwadamilareelesho@gmail.com>
2023-02-08 09:43:03 +0100

    Add project files.

---
+++ ElsoftRestaurant.Services.ProductsAPI/Data/MyDbContext.cs
@@ -0,0 +1,17 @@
+﻿using ElsoftRestaurant.Services.ProductsAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ElsoftRestaurant.Services.ProductsAPI.Data
+{
+    public class MyDbContext: DbContext
+    {
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }
+
+
+
+    }
+}
